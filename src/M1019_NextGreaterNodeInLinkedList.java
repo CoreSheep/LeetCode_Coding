6108fc6d71787b24dde7b78ac5f0fcfc6fd0ee18
Sheep Core <leepaul168@gmail.com>
2019-04-12 20:41:45 +0800

    Transform linkedlist into array, then do the same stuff

---
+++ src/M1019_NextGreaterNodeInLinkedList.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class M1019_NextGreaterNodeInLinkedList {
+
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> A = new ArrayList<>();
+        for (ListNode node = head; node != null; node = node.next)
+            A.add(node.val);
+        int[] res = new int[A.size()];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < A.size(); ++i) {
+            while (!stack.isEmpty() && A.get(stack.peek()) < A.get(i))
+                res[stack.pop()] = A.get(i);
+            stack.push(i);
+        }
+        return res;
+    }
+}
