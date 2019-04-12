5d84ba06c60df83dff9ffe34b8d8fccf6c0a9906
Sheep Core <leepaul168@gmail.com>
2019-04-12 20:43:56 +0800

    Binary tree inorder traversal

---
+++ src/M94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,29 @@
+import sun.reflect.generics.tree.Tree;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Treenode{
+    int val;
+    Treenode left;
+    Treenode right;
+    Treenode (int value) { val = value; }
+}
+public class M94_BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(Treenode root) {
+        Stack<Treenode> stack = new Stack<Treenode>();
+        List<Integer> res = new ArrayList<>();
+        while (root != null || !stack.isEmpty()){
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
